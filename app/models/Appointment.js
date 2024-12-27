634eba6a33a32e142d60ddf4306810f61e6a1121
sadini kumarasiri <sadini95kumarasiri@gmail.com>
2024-12-27 11:55:56 +0530

    minor changes doctor schema

--- app/models/Appointment.js
+++ app/models/Appointment.js
@@ -2,8 +2,7 @@ const mongoose = require('mongoose');
 
 const AppointmentSchema = new mongoose.Schema({
     doctorId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Doctor',
+        type: String,
         required: [true, "Doctor ID is required"],
     },
     patientId: {
